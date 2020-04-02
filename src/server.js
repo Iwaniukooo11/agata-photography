fcaedb30544830c8c6c4991b6c7b355ef3361271
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-04-02 17:09:26 +0200

    go back to polka

--- src/server.js
+++ src/server.js
@@ -13,21 +13,21 @@ import bodyParser from "body-parser";
 const FileStore = sessionFileStore(session);
 
 const { PORT, NODE_ENV } = process.env;
-// const dev = NODE_ENV === "development";
+const dev = NODE_ENV === "development";
 
-// polka(); // You can also use Express
-// express()
-//   .use(
-//     // "/photography",
-//     compression({ threshold: 0 }),
-//     // sirv("static", { dev }),
-//     sirv("static", { dev: true }),
-//     sapper.middleware()
-//   )
-//   .listen(PORT, err => {
-//     if (err) console.log("error", err);
-//   });
+polka(); // You can also use Express
+express()
+  .use(
+    // "/photography",
+    compression({ threshold: 0 }),
+    // sirv("static", { dev }),
+    sirv("static", { dev: true }),
+    sapper.middleware()
+  )
+  .listen(PORT, err => {
+    if (err) console.log("error", err);
+  });
 
-const app = express();
-const dist = "../export";
-app.use(express.static(dist));
+// const app = express();
+// const dist = "../export";
+// app.use(express.static(dist));
