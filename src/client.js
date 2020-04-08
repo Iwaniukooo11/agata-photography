63d302a8e6600250980874935636483db3c527b0
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-04-08 21:58:09 +0200

    dist

--- src/client.js
+++ src/client.js
@@ -1,10 +1,10 @@
 import * as sapper from "@sapper/app";
 
 sapper.start({
-  target: document.querySelector("#sapper")
+  target: document.querySelector("#sapper"),
 });
 
 window.prefetchRoutes = () =>
-  sapper.prefetchRoutes(["/graphic", "copywriting", "photography"]);
-window.prefetch = href => sapper.prefetch(href);
-window.goto = href => sapper.goto(href);
+  // sapper.prefetchRoutes(["/graphic", "copywriting", "photography"]);
+  (window.prefetch = (href) => sapper.prefetch(href));
+window.goto = (href) => sapper.goto(href);
