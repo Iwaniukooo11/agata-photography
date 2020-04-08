1aab3e249ee7a08bd9406eb122356c4fab48b7a1
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-04-08 22:58:06 +0200

    remove unused packs from server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,16 @@
 import sirv from "sirv";
 // import serve from "serve";
 import polka from "polka";
-const path = require("path");
-import express from "express";
+// const path = require("path");
+// import express from "express";
 import compression from "compression";
 import * as sapper from "@sapper/server";
 
-import session from "express-session";
-import sessionFileStore from "session-file-store";
+// import session from "express-session";
+// import sessionFileStore from "session-file-store";
 // import bodyParser from "body-parser";
 
-const FileStore = sessionFileStore(session);
+// const FileStore = sessionFileStore(session);
 
 const { PORT, NODE_ENV } = process.env;
 const dev = NODE_ENV === "development"; // You can also use Express
