98ddd5e27d3e06eb86149c188f51f569a23239da
Iwaniukooo11 <mateusz.iwaniuk11@gmail.com>
2020-04-08 23:16:55 +0200

    change some stuff

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,136 @@
+// import resolve from "@rollup/plugin-node-resolve";
+// import replace from "@rollup/plugin-replace";
+// import commonjs from "@rollup/plugin-commonjs";
+// import svelte from "rollup-plugin-svelte";
+// import babel from "rollup-plugin-babel";
+// import { terser } from "rollup-plugin-terser";
+// import config from "sapper/config/rollup.js";
+// import pkg from "./package.json";
+
+// // import { config as envConfig } from "dotenv";
+
+// import sveltePreprocess from "svelte-preprocess";
+// const preprocess = sveltePreprocess({
+//   scss: {
+//     includePaths: ["src"],
+//   },
+//   postcss: {
+//     plugins: [require("autoprefixer")],
+//   },
+// });
+
+// const mode = process.env.NODE_ENV;
+// const dev = mode === "development";
+// const legacy = !!process.env.SAPPER_LEGACY_BUILD;
+
+// const onwarn = (warning, onwarn) =>
+//   (warning.code === "CIRCULAR_DEPENDENCY" &&
+//     /[/\\]@sapper[/\\]/.test(warning.message)) ||
+//   onwarn(warning);
+
+// export default {
+//   client: {
+//     input: config.client.input(),
+//     output: config.client.output(),
+//     plugins: [
+//       replace({
+//         "process.browser": true,
+//         "process.env.NODE_ENV": JSON.stringify(mode),
+//         // process: JSON.stringify({
+//         //   env: {
+//         //     ...envConfig().parsed, // attached the .env config
+//         //   },
+//         // }),
+//       }),
+//       svelte({
+//         dev,
+//         hydratable: true,
+//         emitCss: true,
+//         preprocess,
+//       }),
+//       resolve({
+//         browser: true,
+//         dedupe: ["svelte"],
+//       }),
+//       commonjs(),
+
+//       legacy &&
+//         babel({
+//           extensions: [".js", ".mjs", ".html", ".svelte"],
+//           runtimeHelpers: true,
+//           exclude: ["node_modules/@babel/**"],
+//           presets: [
+//             [
+//               "@babel/preset-env",
+//               {
+//                 targets: "> 0.25%, not dead",
+//               },
+//             ],
+//           ],
+//           plugins: [
+//             "@babel/plugin-syntax-dynamic-import",
+//             [
+//               "@babel/plugin-transform-runtime",
+//               {
+//                 useESModules: true,
+//               },
+//             ],
+//           ],
+//         }),
+
+//       !dev &&
+//         terser({
+//           module: true,
+//         }),
+//     ],
+
+//     onwarn,
+//   },
+
+//   server: {
+//     input: config.server.input(),
+//     output: config.server.output(),
+//     plugins: [
+//       replace({
+//         "process.browser": false,
+//         "process.env.NODE_ENV": JSON.stringify(mode),
+//       }),
+//       svelte({
+//         generate: "ssr",
+//         dev,
+//         preprocess,
+//       }),
+//       resolve({
+//         // dedupe: ["svelte"]
+//         preferBuiltins: true,
+//       }),
+//       // commonjs()
+//     ],
+//     external: Object.keys(pkg.dependencies).concat(
+//       require("module").builtinModules ||
+//         Object.keys(process.binding("natives"))
+//     ),
+//     // external: ["sirv", "polka"],
+
+//     onwarn,
+//   },
+
+//   serviceworker: {
+//     input: config.serviceworker.input(),
+//     output: config.serviceworker.output(),
+//     plugins: [
+//       resolve(),
+//       replace({
+//         "process.browser": true,
+//         "process.env.NODE_ENV": JSON.stringify(mode),
+//       }),
+//       commonjs(),
+//       !dev && terser(),
+//     ],
+
+//     onwarn,
+//   },
+// };
 import resolve from "@rollup/plugin-node-resolve";
 import replace from "@rollup/plugin-replace";
 import commonjs from "@rollup/plugin-commonjs";
@@ -6,8 +139,6 @@ import babel from "rollup-plugin-babel";
 import { terser } from "rollup-plugin-terser";
 import config from "sapper/config/rollup.js";
 import pkg from "./package.json";
-
-// import { config as envConfig } from "dotenv";
 
 import sveltePreprocess from "svelte-preprocess";
 const preprocess = sveltePreprocess({
@@ -36,11 +167,6 @@ export default {
       replace({
         "process.browser": true,
         "process.env.NODE_ENV": JSON.stringify(mode),
-        // process: JSON.stringify({
-        //   env: {
-        //     ...envConfig().parsed, // attached the .env config
-        //   },
-        // }),
       }),
       svelte({
         dev,
